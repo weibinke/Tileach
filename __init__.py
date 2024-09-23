c80e919912a47ee21458551e239ccd20baa79fa3
weibinke <283142081@qq.com>
2024-09-23 13:17:55 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # Tileach's abbreviation is: | TL or tl
-# URL=: | https://tileach.com
-# Import the required libraries
+# URL=: | https://github.com/weibinke/Tileach
+# Import the Tileach librariy's all code
 
 from .vit import (
     vitocode,
@@ -10,11 +10,16 @@ from .vit import (
     TiCV
 )
 
+from .lib import(
+    lib
+)
+
 __all__ = [
     "vitocode"
     "DataVito",
     "learn",
-    "TiCV"
+    "TiCV",
+    "lib"
 ]
 
 __version__ = "0.0.1"
