e9a4b0870e4d6ba01c4e0c376259e8289e779929
weibinke <283142081@qq.com>
2024-09-23 13:32:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
 from setuptools import setup
 
 
-setup()
+setup(
+    name='Tileach',
+    version='0.0.1',
+    author='weibinke',
+    author_email='283142081@qq.com',
+    url='https://github.com/weibinke/Tileach',
+    packages=[
+        'setuptools',
+        'requests',
+        "re",
+        "opencv",
+        "tensorflow",
+        "sklearn",
+        "oneflow",
+        "numpy",
+        "warnings",
+        "os",
+        "sys"
+    ],
+    install_libraries=[
+        'setuptools',
+        'requests',
+        "re",
+        "opencv",
+        "tensorflow",
+        "sklearn",
+        "oneflow",
+        "numpy",
+        "warnings",
+        "os",
+        "sys"
+    ]
+)
